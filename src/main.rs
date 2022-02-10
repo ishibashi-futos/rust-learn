97251497f6ba0162da4a295bcd409bac15392858
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-10 22:16:43 +0900

    rustの関数について勉強 https://doc.rust-jp.rs/book-ja/ch03-03-how-functions-work.html - 関数には引数が指定できる - fnキーワードで関数を作成できる - 戻り値の型は引数を指定した後 `() -> i32` のように指定する - 関数には文と式が含まれる   - 文 `let y = 6;` のように、値を返さないもの   - 式 `5 + 1` のように、値を返すもの - returnするには `return` 句を使うか、式を最後に記述すればよい

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod guess_the_number;
 mod mutate;
 mod data_types;
+mod functions;
 
 fn main() {
     println!("Hello, world!");
     guess_the_number::guess_the_number();
     mutate::mutate();
     data_types::data_types();
+    functions::all();
     println!("Good bye!");
 }
