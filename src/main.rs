6bf959aec363a53722492c11fedf2169c364b7fc
ishibashi-futos <ishibashi.futos@jp.fujitsu.com>
2022-02-10 14:52:38 +0900

    データ型に関する学習結果を追加 https://doc.rust-jp.rs/book-ja/ch03-02-data-types.html

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod guess_the_number;
 mod mutate;
+mod data_types;
 
 fn main() {
     println!("Hello, world!");
     guess_the_number::guess_the_number();
     mutate::mutate();
+    data_types::data_types();
     println!("Good bye!");
 }
