92bf139753912d406e778cc99fd19b95deda9f80
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-12 08:40:17 +0900

    enum定義の追加方法を学んだ https://doc.rust-jp.rs/book-ja/ch06-01-defining-an-enum.html
    - enumの列挙値に型を持たせることができる
    - `impl`でメソッドを追加できる
    - Option型が使用できる（他言語と似てる）
    - Noneを定義する場合は明確な型を指定する`xxx: Option<type>`

--- src/defining_enum.rs
+++ src/defining_enum.rs
@@ -1,36 +1,57 @@
 pub fn defining_enum() {
-    let v4 = IpAddrKind::V4;
-    let v6 = IpAddrKind::V6;
+    let home = IpAddr::V4(127, 0, 0, 1);
+    home.print();
+    let loopback = IpAddr::V6(String::from("::1"));
+    loopback.print();
 
-    let home = IpAddr::home();
-    let loopback = IpAddr::loopback();
+    let some_number = Some(5);
+    println!("{:?}", some_number);
+    let some_string = Some("a string");
+    println!("{:?}", some_string);
+    let absent_number: Option<i32> = None;
+    println!("{:?}", absent_number);
+
+    let quit = Message::Quit;
+    quit.call();
+    let mv = Message::Move(MoveMessage {x: 10, y: 10});
+    mv.call();
+    let write = Message::Write(String::from("hello"));
+    write.call();
+    let change_color = Message::ChangeColor(255, 0, 255);
+    change_color.call();
 }
 
-enum IpAddrKind {
-    V4,
-    V6,
-}
 
-fn route(ip_type: IpAddrKind) {
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 impl IpAddr {
-    fn home() -> IpAddr {
-        IpAddr {
-            kind: IpAddrKind::V4,
-            address: String::from("127.0.0.1"),
+    fn print(&self) {
+        match self {
+            IpAddr::V4(a, b, c, d) => println!("{}.{}.{}.{}", a, b, c, d),
+            IpAddr::V6(addr) => println!("{}", addr),
         }
     }
+}
 
-    fn loopback() -> IpAddr {
-        IpAddr {
-            kind: IpAddrKind::V6,
-            address: String::from("::1"),
-        }
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move(MoveMessage),
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+#[derive(Debug)]
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
     }
 }
