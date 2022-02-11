9ea8ad2a27688d563eb0cb81825671c59bb0561e
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-11 18:08:47 +0900

    構造体に処理を追加する方法を学んだ https://doc.rust-jp.rs/book-ja/ch05-03-method-syntax.html
    - インスタンスメソッドは、最初の引数に`&self`で宣言する
      - `&self`の後ろにパラメータを続けることができる
    - staticなメソッドは、`構造体名::メソッド名`で呼び出せる？
    - `object.some_func()`を呼び出すと、コンパイラが自動でオブジェクトに`&`, `&mut`, `*` のいずれかを自動的に付与してくれる
    - implブロックは複数に分割することができるらしい

--- src/example_struct.rs
+++ src/example_struct.rs
@@ -11,6 +11,22 @@ pub fn example_struct() {
 
     println!("rect is {:?}", rect);
     println!("rect is {:#?}", rect); // #で指定すると、構造体のプロパティ名を整形して表示する
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(10);
+    square.print_area();
+    println!("square is {}", square.is_square());
 }
 
 // debugトレイトを使用する
@@ -22,4 +38,31 @@ struct Rectangle {
 
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
+}
+
+impl Rectangle {
+    // Rectangle構造体にareaメソッドを定義する
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn print_area(&self) {
+        println!("The area of the rectangle is {} square pixels.", self.area());
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+}
+
+// implブロックは分割することができる
+impl Rectangle {
+    fn is_square(&self) -> bool {
+        self.width == self.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::example_struct::example_struct;
+
 mod guess_the_number;
 mod mutate;
 mod data_types;
@@ -7,6 +9,7 @@ mod ownership;
 mod references_and_borrowing;
 mod slices;
 mod use_struct;
+mod example_struct;
 
 fn main() {
     println!("Hello, world!");
@@ -19,5 +22,6 @@ fn main() {
     references_and_borrowing::references_and_borrowing();
     slices::slices();
     use_struct::use_struct();
+    example_struct::example_struct();
     println!("Good bye!");
 }
