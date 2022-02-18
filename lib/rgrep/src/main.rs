d1db9db4cd140ba677a6b339a4c5e2896cec8353
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-18 23:00:43 +0900

    適切なエラー処理を実装
    - 引数の受け取り方をConfig構造体に委任
    - Config構造体はErrorかConfigを返す
    - エラーの型が`&'static str`になってるけど理由はなんだろうか・・・
    - 成功の型がない場合`()`を使用する
    - Resultでエラーのみを評価したい場合`if let Error()`でエラーの場合の処理を書ける

--- lib/rgrep/src/main.rs
+++ lib/rgrep/src/main.rs
@@ -1,19 +1,50 @@
-use std::{env, fs::File, io::Read};
+use std::error::Error;
 use std::io::prelude::*;
+use std::process;
+use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(-1);
+    });
 
-    println!("Searching for {} in {}", query, filename);
+    println!("Searching for {} in {}", config.query, config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    // Errorを返した場合はIfステート内の処理を行う
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
