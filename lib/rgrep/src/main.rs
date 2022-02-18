38864e6c9e83d9178432624b9b95582c7bbafd9c
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-18 23:07:41 +0900

    メインコードをライブラリクレートに移植しました https://doc.rust-jp.rs/book-ja/ch12-03-improving-error-handling-and-modularity.html
    - `extern crate`でlib.rsの中身を読み込める

--- lib/rgrep/src/main.rs
+++ lib/rgrep/src/main.rs
@@ -1,7 +1,9 @@
-use std::error::Error;
-use std::io::prelude::*;
+extern crate rgrep;
+
 use std::process;
-use std::{env, fs::File, io::Read};
+use std::env;
+
+use rgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,36 +17,8 @@ fn main() {
     println!("Searching for {} in {}", config.query, config.filename);
 
     // Errorを返した場合はIfステート内の処理を行う
-    if let Err(e) = run(config) {
+    if let Err(e) = rgrep::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
