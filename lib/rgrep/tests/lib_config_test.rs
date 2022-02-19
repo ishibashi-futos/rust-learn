bc2bfeece81a16e0e97d2d633756ffe97378c1a7
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-19 18:25:27 +0900

    configクラスにテストを追加

---
+++ lib/rgrep/tests/lib_config_test.rs
@@ -0,0 +1,30 @@
+extern crate rgrep;
+
+use rgrep::Config;
+
+#[test]
+fn new_argslength2_is_ok() {
+    let args = vec![
+      String::from("rgrep"),
+      String::from("foo"),
+      String::from("bar"),
+    ];
+
+    let config = Config::new(&args).unwrap();
+    assert_eq!(config.query, "foo");
+    assert_eq!(config.filename, "bar");
+}
+
+#[test]
+fn new_args_length_less_than_2_should_be_error() {
+    let args = vec![
+        String::from("rgrep"),
+        String::from("foo"),
+    ];
+
+    if let Err(str) = Config::new(&args) {
+        assert_eq!(str, "not enough arguments");
+    } else {
+        panic!("Should be error");
+    }
+}
