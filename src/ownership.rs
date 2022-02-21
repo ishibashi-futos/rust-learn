e3a3ac925d4393a4f8b59095f9210af6de068850
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-21 18:39:23 +0900

    ブロックの確認のためのコードを追加
    - ブロック内で構造体の値を触っても所有権は移らない
    - ブロック内で同じ変数名で上書きをしたとしても、親ブロックの変数に変更は起こらない
    - pushされるときに所有権が移るので、ブロックが終了しても変数は残る

--- src/ownership.rs
+++ src/ownership.rs
@@ -26,3 +26,46 @@ fn tasks_ownership(some_string: String) {
 fn makes_copy(some_integer: i32) {
     println!("some_integer: {}", &some_integer);
 }
+
+#[derive(PartialEq, Debug)]
+struct MyValue {
+    item: u32,
+}
+
+#[allow(dead_code)]
+impl MyValue {
+    fn new(item: u32) -> MyValue {
+        MyValue { item }
+    }
+
+    fn add(&self, b: u32) -> MyValue {
+        MyValue {
+            item: self.item + b,
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn append_values(values: &mut Vec<MyValue>) {
+    let mut a = MyValue::new(100);
+    {
+        a.item = 130;
+        let a = a.add(32);
+        values.push(a);
+    }
+    values.push(a);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn values() {
+        let mut values: Vec<MyValue> = Vec::new();
+
+        append_values(&mut values);
+
+        assert_eq!(values, vec![MyValue::new(162), MyValue::new(130),]);
+    }
+}
