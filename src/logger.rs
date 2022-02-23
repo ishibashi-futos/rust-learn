1cfc5c6fc3eed7753058a8f490440a731246d1d6
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-23 21:57:55 +0900

    処理時間を測るためのtimerを実装

--- src/logger.rs
+++ src/logger.rs
@@ -2,3 +2,16 @@ use chrono::Local;
 pub fn info(msg: &str) {
     println!("{}\tINFO\t{}", Local::now(), msg);
 }
+
+pub mod timer {
+    use super::*;
+
+    pub fn v1(name: &str, f: Box<dyn Fn(Box<dyn Fn(&str) -> ()>) -> ()>) {
+        let start = Local::now();
+        info(&format!("TIMER\t[{}]\tTimer Begin: {}", name, start));
+        f(Box::new(|msg| info(&format!("TIMER\t{}", msg))));
+        let end = Local::now();
+        info(&format!("TIMER\t[{}]\tTimer End  : {}", name, start));
+        info(&format!("TIMER\t[{}]\tDuration   : {}", name, end - start));
+    }
+}
