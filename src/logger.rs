6a561e8188d4d565028d8bd4f56e356c45f7d1cd
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-24 21:25:47 +0900

    #5 loggerにnameを表示する
    - `&'static`で受け取るだけではダメ
    - 内部で生成するlogger関数に変数をmoveしないといけない

--- src/logger.rs
+++ src/logger.rs
@@ -6,12 +6,26 @@ pub fn info(msg: &str) {
 pub mod timer {
     use super::*;
 
+    #[allow(dead_code)]
     pub fn v1(name: &str, f: Box<dyn Fn(Box<dyn Fn(&str) -> ()>) -> ()>) {
         let start = Local::now();
         info(&format!("TIMER\t[{}]\tTimer Begin: {}", name, start));
         f(Box::new(|msg| info(&format!("TIMER\t{}", msg))));
         let end = Local::now();
-        info(&format!("TIMER\t[{}]\tTimer End  : {}", name, start));
+        info(&format!("TIMER\t[{}]\tTimer End  : {}", name, end));
+        info(&format!("TIMER\t[{}]\tDuration   : {}", name, end - start));
+    }
+
+    #[allow(dead_code)]
+    pub fn v2(name: &'static str, f: Box<dyn Fn(Box<dyn Fn(&str) -> ()>)>) {
+        let start = Local::now();
+        info(&format!("TIMER\t[{}]\tTimer Begin: {}", name, start));
+        // captureした値をmoveする事で、メンバ関数内に実態として値をキャプチャできる（参照ではないのでライフタイムが関係ない）
+        f(Box::new(move |msg| {
+            info(&format!("TIMER\t[{}]\t{}", name, msg))
+        }));
+        let end = Local::now();
+        info(&format!("TIMER\t[{}]\tTimer End  : {}", name, end));
         info(&format!("TIMER\t[{}]\tDuration   : {}", name, end - start));
     }
 }
