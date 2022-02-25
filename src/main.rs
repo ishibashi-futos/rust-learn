67989ff4e59af4dbb36fef6f930fd24a84cb47ad
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-26 08:53:41 +0900

    Welcome to AtCoderの結果を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let line1 = input.trim().parse::<i32>().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let line2: i32 = input
+        .trim()
+        .split(" ")
+        .map(|x| x.parse::<i32>().unwrap())
+        .sum();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let line3 = input.trim();
+    println!("{} {}", line1 + line2, line3);
 }
