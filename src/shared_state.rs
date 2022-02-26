e36d7d3d8d675721da41159dbeb3e092a4d7a0cb
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-26 09:17:34 +0900

    Mutex/Arcでスレッド間でメモリ上のデータを共有する方法を学んだ https://doc.rust-jp.rs/book-ja/ch16-03-shared-state.html
    - Mutexで囲んだデータはlockする事で使用・書き込みできる
    - スレッド内に持ち込む場合は参照モデル？であるArcを使用する（Atomic Rc?）
    - lockされたデータはブロックを抜ける際にデストラクタで暗黙的にアンロックされている

---
+++ src/shared_state.rs
@@ -0,0 +1,36 @@
+use std::sync::{Mutex, Arc}; // 代わりにArcを使用する
+// use std::rc::Rc; // Rcはスレッドで値を共有するために安全ではないのでコンパイルエラーになる
+use std::thread;
+
+use crate::logger;
+
+pub fn shared_state() {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    logger::info(&format!("m = {:?}", m));
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            logger::info(&format!("thread {:?}: {}", thread::current().id(), *num));
+            *num += 1;
+            // デストラクタで暗黙的にアンロックされている？
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    logger::info(&format!("Result: {}", *counter.lock().unwrap()));
+}
