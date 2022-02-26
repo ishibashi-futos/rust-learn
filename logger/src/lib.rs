a99eff1c77e76f2ab30bf55975ab77bd49236655
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-26 23:09:25 +0900

    loggerを外部ライブラリ化してみた

---
+++ logger/src/lib.rs
@@ -0,0 +1,83 @@
+mod spinners;
+
+pub mod logger {
+    use chrono::Local;
+
+    pub fn info(msg: &str) {
+        println!("{}\tINFO\t{}", Local::now(), msg);
+    }
+
+    pub mod timer {
+        use super::*;
+
+        #[allow(dead_code)]
+        pub fn v1(name: &str, f: Box<dyn Fn(Box<dyn Fn(&str) -> ()>) -> ()>) {
+            let start = Local::now();
+            info(&format!("TIMER\t[{}]\tTimer Begin: {}", name, start));
+            f(Box::new(|msg| info(&format!("TIMER\t{}", msg))));
+            let end = Local::now();
+            info(&format!("TIMER\t[{}]\tTimer End  : {}", name, end));
+            info(&format!("TIMER\t[{}]\tDuration   : {}", name, end - start));
+        }
+
+        #[allow(dead_code)]
+        pub fn v2(name: &'static str, f: Box<dyn Fn(Box<dyn Fn(&str) -> ()>)>) {
+            let start = Local::now();
+            info(&format!("TIMER\t[{}]\tTimer Begin: {}", name, start));
+            // captureした値をmoveする事で、メンバ関数内に実態として値をキャプチャできる（参照ではないのでライフタイムが関係ない）
+            f(Box::new(move |msg| {
+                info(&format!("TIMER\t[{}]\t{}", name, msg))
+            }));
+            let end = Local::now();
+            info(&format!("TIMER\t[{}]\tTimer End  : {}", name, end));
+            info(&format!("TIMER\t[{}]\tDuration   : {}", name, end - start));
+        }
+    }
+
+    pub mod spinner {
+        use std::{
+            io::{stdout, Write},
+            sync::mpsc,
+            thread::JoinHandle,
+            time::Duration,
+        };
+
+        use crate::spinners::spinners;
+
+        use super::*;
+
+        pub fn v1(name: &'static str, f: Box<dyn Fn() -> () + Send>) {
+            let start = Local::now();
+            info(&format!("TIMER\t[{}]\tTimer Begin: {}", name, start));
+            let (sender, reciever) = mpsc::channel();
+            let _ = std::thread::spawn(move || {
+                f();
+                let end = Local::now();
+                sender.send(end)
+            });
+
+            let handler = do_spinner(name);
+
+            let end = reciever.recv().unwrap(); // fnが終了するのを待つ
+
+            drop(handler); // dropする事で強制的にスレッドを終了させる
+            print!("\r"); // Spinnerを消す
+            info(&format!("TIMER\t[{}]\tTimer End  : {}", name, end));
+            info(&format!("TIMER\t[{}]\tDuration   : {}", name, end - start));
+        }
+
+        fn do_spinner(name: &'static str) -> JoinHandle<()> {
+            std::thread::spawn(move || {
+                let mut spinner = spinners::Dots::new();
+                // let mut spinner = spinners::Moon::new();
+                // let mut spinner = spinners::Clock::new();
+                let mut stdout = stdout();
+                loop {
+                    print!("\r{} Processing - [{}]", spinner.next().unwrap(), name);
+                    stdout.flush().unwrap();
+                    std::thread::sleep(Duration::from_millis(100));
+                }
+            })
+        }
+    }
+}
---
+++ logger/src/spinners.rs
@@ -0,0 +1,64 @@
+pub mod spinners {
+    pub struct Dots {
+        dots: Vec<char>
+    }
+
+    impl Dots {
+        #[allow(dead_code)]
+        pub fn new() -> Dots {
+            Dots {
+                dots: vec!['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']
+            }
+        }
+    }
+
+    impl Iterator for Dots {
+        type Item = char;
+        fn next(&mut self) -> Option<Self::Item> {
+            self.dots.rotate_left(1);
+            Some(self.dots[0])
+        }
+    }
+
+    pub struct Moon {
+        item: Vec<char>,
+    }
+
+    impl Moon {
+        #[allow(dead_code)]
+        pub fn new() -> Moon {
+            Moon {
+                item: vec!['🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘']
+            }
+        }
+    }
+
+    impl Iterator for Moon {
+        type Item = char;
+        fn next(&mut self) -> Option<Self::Item> {
+            self.item.rotate_left(1);
+            Some(self.item[0])
+        }
+    }
+
+    pub struct Clock {
+        item: Vec<char>,
+    }
+
+    impl Clock {
+        #[allow(dead_code)]
+        pub fn new() -> Clock {
+            Clock {
+                item: vec!['🕐', '🕑', '🕒', '🕓', '🕔', '🕕', '🕖', '🕗', '🕘', '🕙', '🕚', '🕛']
+            }
+        }
+    }
+
+    impl Iterator for Clock {
+        type Item = char;
+        fn next(&mut self) -> Option<Self::Item> {
+            self.item.rotate_left(1);
+            Some(self.item[0])
+        }
+    }
+}
