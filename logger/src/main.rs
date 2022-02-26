ab7c7614314c34b61a8324561b6cd60a3daec7e5
ishibashi-futos <ishibashi.futos@outlook.com>
2022-02-26 23:10:20 +0900

    fix format

--- logger/src/main.rs
+++ logger/src/main.rs
@@ -1,12 +1,12 @@
-use std::time::Duration;
-
 extern crate logger;
 
 fn main() {
-    logger::logger::spinner::v1("spinner", Box::new(|| {
-        for _ in 0..=100 {
-            std::thread::sleep(std::time::Duration::from_millis(100));
-        }
-    }));
-
+    logger::logger::spinner::v1(
+        "spinner",
+        Box::new(|| {
+            for _ in 0..=100 {
+                std::thread::sleep(std::time::Duration::from_millis(100));
+            }
+        }),
+    );
 }
