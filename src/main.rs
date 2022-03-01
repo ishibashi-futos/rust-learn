31cdb33766a34ad517e550bc45945df2361fd80c
ishibashi-futos <ishibashi.futos@outlook.com>
2022-03-01 23:18:02 +0900

    abc241 A - Digit Machineの結果を提出 https://atcoder.jp/contests/abc241

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,14 @@ use std::io;
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let line1 = input.trim().parse::<i32>().unwrap();
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let line2: i32 = input
+    let nums = input
         .trim()
         .split(" ")
         .map(|x| x.parse::<i32>().unwrap())
-        .sum();
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let line3 = input.trim();
-    println!("{} {}", line1 + line2, line3);
+        .collect::<Vec<i32>>();
+
+    let second = nums.first().unwrap();
+    let third = nums.get(*second as usize).unwrap();
+    let fourth = nums.get(*third as usize).unwrap();
+    println!("{}", fourth);
 }
