d7e1ab3c00cf3b0a3290d4d523128e29ede449b2
ishibashi-futos <ishibashi.futos@outlook.com>
2022-03-04 20:19:43 +0900

    DBを共有する方法を学んだ

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,32 @@ use mini_redis::{Connection, Frame};
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 
+
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use bytes::Bytes;
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
 #[tokio::main]
 pub async fn main() {
 
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     // selfでmini_redis::Connectionを参照することができる
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
 
@@ -28,10 +35,12 @@ async fn process(socket: TcpStream) {
         println!("Got: {:?}", frame);
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".into())
             },
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
